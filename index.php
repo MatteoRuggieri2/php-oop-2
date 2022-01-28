232cfe9831c075a9e3426dbd6c5155b73dda1b31
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-28 16:13:31 +0100

    Aggiunti TechItem

--- index.php
+++ index.php
@@ -10,6 +10,12 @@ Provate a stampare in pagina come visto questa mattina i prodotti scelti dall'ut
 
 <?php 
 
-require __DIR__ . '/Product.php';
+require_once __DIR__ . '/TechItem.php';
 
+$mouse = new TechItem('Corsair M65 ELITE', 69.99, 50);
+$mouse->vote = 4;
+var_dump($mouse);
+
+$keyboard = new TechItem('Corsair K70 RGB MK.2', 179.99, 75);
+var_dump($keyboard);
 ?>
